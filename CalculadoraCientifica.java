3bdd833a1d8c76fcdebd12dac7d51e4237e36ca1
guilhermeken <103258737+guilhermeken@users.noreply.github.com>
2022-06-01 15:23:31 -0300

    Sobrecarga de métodos

--- CalculadoraCientifica.java
+++ CalculadoraCientifica.java
@@ -1,15 +1,15 @@
 public class CalculadoraCientifica {
     public void potencia(byte a, byte b){
-        System.out.println(Math.pow(Double)(a, b));
+        System.out.println(Math.pow((double)a, b));
     }
     public void potencia (String s1, String s2){
-        System.out.println(Math.pow((Double.parseDouble(s1), Double.parseDouble(s2));
+        System.out.println(Math.pow(Double.parseDouble(s1), Double.parseDouble(s2)));
     }
     public void potencia (int i1, double d1){
-        System.out.println(Math.pow((Double)i1, d1));
+        System.out.println(Math.pow((double)i1, d1));
     }
     public void raiz (int i1){
-        System.out.println(Math.sqrt((Double)(i1));
+        System.out.println(Math.sqrt((double)(i1)));
     }
     public void raiz (double d1){
         System.out.println(Math.sqrt(d1));
