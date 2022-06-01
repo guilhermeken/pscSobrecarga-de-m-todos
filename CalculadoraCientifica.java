27e3fed021ff17d1872a3841e2b92ca45394979d
guilhermeken <103258737+guilhermeken@users.noreply.github.com>
2022-06-01 15:19:02 -0300

    Update CalculadoraCientifica.java

--- CalculadoraCientifica.java
+++ CalculadoraCientifica.java
@@ -1,12 +1,12 @@
 public class CalculadoraCientifica {
     public void potencia(byte a, byte b){
-        System.out.println(Math.pow(a, b));
+        System.out.println(Math.pow(Double)(a, b));
     }
     public void potencia (String s1, String s2){
-        System.out.println(Math.pow(Double.parseDouble(s1), Double.parseDouble(s2)));
+        System.out.println(Math.pow((Double.parseDouble(s1), Double.parseDouble(s2));
     }
     public void potencia (int i1, double d1){
-        System.out.println(Math.pow(i1, d1));
+        System.out.println(Math.pow((Double)i1, d1));
     }
     public void raiz (int i1){
         System.out.println(Math.sqrt((Double)(i1));
